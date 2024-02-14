4dc024707c7e8f5f5f143c03a4d3204f79415e34
auntyemman <1mole.coco@gmail.com>
2024-02-14 12:05:39 +0100

    feat(): verify registration email

--- src/controllers/userAuth/index.ts
+++ src/controllers/userAuth/index.ts
@@ -1,7 +1,5 @@
 import { signUp } from './signUp';
 import { signIn } from './signIn';
+import { verifyEmail } from './verifyEmail';
 
-export {
-    signUp,
-    signIn
-};
+export { signUp, signIn, verifyEmail };
--- src/routes/userAuth.ts
+++ src/routes/userAuth.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 export const auth: Router = Router();
 
-import {
-    signUp,
-    signIn,
-} from '../controllers/userAuth';
+import { signUp, signIn, verifyEmail } from '../controllers/userAuth';
 
 /**---------------------------------SignUp request----------------------------------------- */
 auth.post('/signup', signUp);
 
 /**---------------------------------SignIn request----------------------------------------- */
 auth.post('/signin', signIn);
+
+/**---------------------------------Verify email request----------------------------------------- */
+auth.get('/verify-email', verifyEmail);
